0496ac48a746df533cf73cb61cd410e099f0844b
benwaples <benwaples@gmail.com>
2021-03-02 12:08:50 -0800

    blockbuster header

--- src/components/blockbuster/BlockBuster.tsx
+++ src/components/blockbuster/BlockBuster.tsx
@@ -1,24 +1,10 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import './BlockBuster.scss';
+import Header from './Header';
 
 function BlockBuster(): JSX.Element {
-  return (
-    <header className="bb-header">
-      <h1>BLOCKBUSTER</h1>
-      <nav>
-        <a href="google.com">HOME</a>
-        <a href="google.com">NEW RELEASES</a>
-        <a href="google.com">SHOP</a>
-        <a href="google.com">ABOUT</a>
-        <a href="google.com">CONTACT</a>
-        <a href="google.com">BLOG</a>
-        <a href="google.com">
-          <img alt="shopping cart" />
-        </a>
-      </nav>
-    </header>
-  );
+  return <Header />;
 }
 
 export default BlockBuster;
