3d3dbf183e53726fb35bdf454329021811de1d69
benwaples <benwaples@gmail.com>
2021-03-03 11:13:40 -0800

    new releases title

--- src/components/blockbuster/newReleases/NewReleases.tsx
+++ src/components/blockbuster/newReleases/NewReleases.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import { newReleaseTitles } from './assets';
+// import ImageGallery from 'react-image-gallery';
+// import { newReleaseTitles } from './assets';
 import './NewReleases.scss';
 
 function NewReleases(): JSX.Element {
   return (
     <div className="new-releases">
       <h1>New Releases</h1>
-      <ImageGallery
+      {/* <ImageGallery
         items={newReleaseTitles}
         showFullscreenButton={false}
         showPlayButton={false}
         showThumbnails={false}
-      />
+      /> */}
     </div>
   );
 }
