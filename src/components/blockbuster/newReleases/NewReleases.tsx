b21557d0bdda8cc7cff7798611c77d79b354a17c
benwaples <benwaples@gmail.com>
2021-03-03 11:28:08 -0800

    new releases slider

--- src/components/blockbuster/newReleases/NewReleases.tsx
+++ src/components/blockbuster/newReleases/NewReleases.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-// import ImageGallery from 'react-image-gallery';
-// import { newReleaseTitles } from './assets';
+import ImageGallery from 'react-image-gallery';
+import { newReleaseTitles } from './assets';
 import './NewReleases.scss';
 
 function NewReleases(): JSX.Element {
   return (
     <div className="new-releases">
       <h1>New Releases</h1>
-      {/* <ImageGallery
-        items={newReleaseTitles}
-        showFullscreenButton={false}
-        showPlayButton={false}
-        showThumbnails={false}
-      /> */}
+      <div className="release-wrapper">
+        <ImageGallery
+          items={newReleaseTitles}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          showThumbnails={false}
+          disableSwipe={false}
+        />
+      </div>
     </div>
   );
 }
--- src/components/blockbuster/newReleases/assets.ts
+++ src/components/blockbuster/newReleases/assets.ts
@@ -3,66 +3,77 @@ export const newReleaseTitles = [
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/random-acts-of-violence-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/random-acts-of-violence-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/redemption-day-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/redemption-day-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/silk-road-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/silk-road-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-last-vermeer-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-last-vermeer-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-croods-a-new-age-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-croods-a-new-age-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/centigrade-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/centigrade-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/scooby-doo-the-sword-and-the-scoob-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/scooby-doo-the-sword-and-the-scoob-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/collective-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/collective-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-swordsman-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/the-swordsman-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/a-call-to-spy-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/a-call-to-spy-cover.jpg',
   },
   {
     original:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/a-call-to-spy-cover.jpg',
+    // originalClass: 'new-release-title',
     thumbnail:
       'https://bendblockbuster.com/wp-content/uploads/2021/02/a-call-to-spy-cover.jpg',
   },
